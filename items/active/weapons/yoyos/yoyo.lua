24e7f44760687844c30a6c3dc3a7bc9729806aee
Bloopsers <unconfigured@null.spigotmc.org>
2018-03-29 13:54:25 -0400

    Revert "Fix cursor"
    This reverts commit cf9977d5d7d3a84f282b26a676c3a79ce3a525d6.

--- items/active/weapons/yoyos/yoyo.lua
+++ items/active/weapons/yoyos/yoyo.lua
@@ -81,7 +81,7 @@ function update(dt, fireMode, shiftHeld, moves)
   trackProjectile()
 
   if self.projectileId then
-    activeItem.setCursor("/cursors/reticle0.cursor")
+    activeItem.setCursor("/cursors/yoyo.cursor")
     setStance("throw")
     if world.entityExists(self.projectileId) then
       for id,counterweight in pairs(counterweights) do
